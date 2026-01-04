35fbf7a270385f7326c3d90ffdaecd9f1c222ddd
[iharshkumar] <[srivastavaharsh1108@gmail.com]>
2026-01-04 20:52:35 +0530

    Login and register related changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'          // Tailwind styles
 import Header from './components/Header/index.jsx';
 import Footer from './components/Footer/index.jsx';
-import Home from './Pages/Home/index.jsx';
-import ProductListing from './Pages/ProductListing/index.jsx';
-import ProductDetails from './Pages/ProductDetails/index.jsx';
+import Home from './Pages/Home';
+import ProductListing from './Pages/ProductListing';
+import ProductDetails from './Pages/ProductDetails';
 import { createContext } from 'react';
 
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 
-import { ProductZoom } from "./components/ProductZoom/index.jsx";
+import { ProductZoom } from "./components/ProductZoom";
 import { IoCloseSharp } from "react-icons/io5";
-import ProductDetailsComponents from "./components/ProductDetails/index.jsx";
+import ProductDetailsComponents from "./components/ProductDetails";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 
 
@@ -45,6 +47,9 @@ function App() {
             <Route path={'/'} exact={true} element={<Home />} />
             <Route path={'/productListing'} exact={true} element={<ProductListing />} />
             <Route path={'/product/:id'} exact={true} element={<ProductDetails />} />
+            <Route path={'/login'} exact={true} element={<Login />} />
+            <Route path={'/register'} exact={true} element={<Register />} />
+
           </Routes>
           <Footer />
         </MyContext.Provider>
@@ -59,22 +64,22 @@ function App() {
         aria-describedby="alert-dialog-description"
         className="productDetailModal"
       >
-        
+
         <DialogContent>
           <div className="flex items-center w-full productDetailModalContainer relative">
             <Button className="w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000]
              !absolute top-[15px] right-[15px] !bg-[#f1f1f1]" onClick={handleCloseProductDetailsModal}>
-              <IoCloseSharp className="text-[20px]"/></Button>
+              <IoCloseSharp className="text-[20px]" /></Button>
             <div className="col1 w-[40%] !px-2">
-              <ProductZoom/>
+              <ProductZoom />
             </div>
 
             <div className="col2 w-[50%] !py-5 !px-8 !pr-16 productContent">
-              <ProductDetailsComponents/>
+              <ProductDetailsComponents />
             </div>
           </div>
         </DialogContent>
-        
+
       </Dialog>
     </>
   )
@@ -82,4 +87,4 @@ function App() {
 
 export default App;
 
-export {MyContext};
+export { MyContext };
