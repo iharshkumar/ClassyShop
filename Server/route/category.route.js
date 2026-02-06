e8c5c1451230dc6c8a90dd5621110430fa2677a4
[iharshkumar] <srivastavaharsh1108@gmail.com>
2026-02-06 23:41:32 +0530

    Adding routes for the category

---
+++ Server/route/category.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import auth from "../middleware/auth.js";
+import upload from '../middleware/multer.js'
+import { createCategory, deleteCategory, getCategories, getCategoriesCount, getCategory, getSubCategoriesCount, removeImageFromCloudinary, updatedCategory, uploadImages } from "../controllers/category.controller.js";
+
+const categoryRouter = Router();
+
+categoryRouter.post('/uploadImages',auth,upload.array('images'),uploadImages);
+categoryRouter.post('/create',auth,createCategory);
+categoryRouter.get('/',getCategories);
+categoryRouter.get('/get/count',getCategoriesCount);
+categoryRouter.get('/get/count/subCat',getSubCategoriesCount);
+categoryRouter.get('/:id',getCategory);
+categoryRouter.delete('/:id',auth,deleteCategory);
+categoryRouter.delete('/deleteImage',auth,removeImageFromCloudinary);
+categoryRouter.put('/:id',auth,updatedCategory);
+
+export default categoryRouter;
