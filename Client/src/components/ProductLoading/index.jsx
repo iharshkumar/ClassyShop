77a4d307ab708df3c34bdefe013e2efca6b7d888
[iharshkumar] <srivastavaharsh1108@gmail.com>
2026-03-07 23:48:42 +0530

    Adding skeleton feature for product loading

---
+++ Client/src/components/ProductLoading/index.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+const ProductLoading = () => {
+    return (
+        <div className='flex items-center gap-3 animate-pulse'>
+            <div className='col w-[16%] h-[250px] !mb-8'>
+                <div className="flex items-center justify-center !mb-3 w-full h-48 bg-gray-200 rounded-md ">
+                    <svg className="w-11 h-11 text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                    </svg>
+                </div>
+
+                <div className="!h-2.5 !bg-gray-200 !rounded-full !w-48 !mb-4"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-56 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-48 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-36 !mb-2"></div>
+            </div>
+
+
+            <div className='col w-[16%] h-[250px] !mb-8'>
+                <div className="flex items-center justify-center !mb-3 w-full h-48 bg-gray-200 rounded-md ">
+                    <svg className="w-11 h-11 text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                    </svg>
+                </div>
+
+                <div className="!h-2.5 !bg-gray-200 !rounded-full !w-48 !mb-4"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-56 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-48 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-36 !mb-2"></div>
+            </div>
+
+
+            <div className='col w-[16%] h-[250px] !mb-8'>
+                <div className="flex items-center justify-center !mb-3 w-full h-48 bg-gray-200 rounded-md ">
+                    <svg className="w-11 h-11 text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                    </svg>
+                </div>
+
+                <div className="!h-2.5 !bg-gray-200 !rounded-full !w-48 !mb-4"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-56 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-48 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-36 !mb-2"></div>
+            </div>
+
+
+            <div className='col w-[16%] h-[250px] !mb-8'>
+                <div className="flex items-center justify-center !mb-3 w-full h-48 bg-gray-200 rounded-md ">
+                    <svg className="w-11 h-11 text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                    </svg>
+                </div>
+
+                <div className="!h-2.5 !bg-gray-200 !rounded-full !w-48 !mb-4"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-56 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-48 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-36 !mb-2"></div>
+            </div>
+
+
+            <div className='col w-[16%] h-[250px] !mb-8'>
+                <div className="flex items-center justify-center !mb-3 w-full h-48 bg-gray-200 rounded-md ">
+                    <svg className="w-11 h-11 text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                    </svg>
+                </div>
+
+                <div className="!h-2.5 !bg-gray-200 !rounded-full !w-48 !mb-4"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-56 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-48 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-36 !mb-2"></div>
+            </div>
+
+
+            <div className='col w-[16%] h-[250px] !mb-8'>
+                <div className="flex items-center justify-center !mb-3 w-full h-48 bg-gray-200 rounded-md ">
+                    <svg className="w-11 h-11 text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                    </svg>
+                </div>
+
+                <div className="!h-2.5 !bg-gray-200 !rounded-full !w-48 !mb-4"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-56 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-48 !mb-2"></div>
+                <div className="!h-2 !bg-gray-200 !rounded-full !w-36 !mb-2"></div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductLoading
